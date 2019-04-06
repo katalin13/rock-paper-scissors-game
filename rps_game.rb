06f3b4151e3dc885a23b66caab3b72d4729ff512
Katalin-Anett Balazs <tk.anett@yahoo.com>
2019-04-07 00:14:14 +0100

    Update rps_game.rb

--- rps_game.rb
+++ rps_game.rb
@@ -9,23 +9,32 @@ def computer_game(player)
   cpu_choice = ['rock', 'paper', 'scissors']
   cpu =cpu_choice.sample
   result = if player == cpu
-    'Draw... Give it another go?'
+    'Draw...'
     
   else
     case player
       when 'rock'
-        cpu == 'scissors' ? 'win' : 'lose'
+        cpu == 'scissors' ? 'You won!' : 'You lose!'
       when 'paper'
-        cpu == 'rock' ? 'win' : 'lose'
+        cpu == 'rock' ? 'You won!' : 'You lose!'
       when 'scissors' 
-        cpu == 'paper' ? 'win' : 'lose'
+        cpu == 'paper' ? 'You won!' : 'You lose!'
     end
   end
   puts "#{cpu}, #{result}"
+  puts 'Again?'
+
+    input = gets.chomp!
+    until input.downcase == 'no'
+      puts 'Ok, here we go again:'
+      choice = gets.chomp!
+      computer_game(choice)
+      puts 'Again?'
+      input = gets.chomp!
+    end
+    puts 'Really sorry to hear that! Bye!'
 end
   
-    
-
 if user_input.downcase == 'yes'
   puts 'Let\'s get it started then!'
   puts
@@ -35,14 +44,3 @@ if user_input.downcase == 'yes'
 else 
   puts 'Really sorry to hear that! Bye!'
 end
-
-puts 'Again?'
-input = gets.chomp!
-until input.downcase == 'no'
-  puts 'Ok, here we go again:'
-  choice = gets.chomp!
-  computer_game(choice)
-  puts 'Again?'
-  input = gets.chomp!
-end
-puts 'Really sorry to hear that! Bye!'

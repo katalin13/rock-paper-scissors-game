0ee825abd10ed8e7cb3b31cdfb0986f62104104f
Katalin-Anett Balazs <tk.anett@yahoo.com>
2019-04-08 18:57:20 +0100

    Update rps_game.rb

--- rps_game.rb
+++ rps_game.rb
@@ -1,7 +1,7 @@
 puts 'Welcome to a game of luck:'
-puts '-'*26
-puts '    Rock Paper Scissor'
-puts 'Your opponent will be The Computer !'
+puts '-'*31
+puts '     Rock Paper Scissor'
+puts 'Your component will be The Computer!'
 puts 'Willing to play?'
 user_input = gets.chomp!
 
@@ -14,18 +14,20 @@ def computer_game(player)
   else
     case player
       when 'rock'
-        cpu == 'scissors' ? 'You won!' : 'You lost!'
+        cpu == 'scissors' ? 'You won!' : 'You lose!'
       when 'paper'
-        cpu == 'rock' ? 'You won!' : 'You lost!'
+        cpu == 'rock' ? 'You won!' : 'You lose!'
       when 'scissors' 
-        cpu == 'paper' ? 'You won!' : 'You lost!'
+        cpu == 'paper' ? 'You won!' : 'You lose!'
     end
   end
+  puts
   puts "#{cpu}, #{result}"
   puts 'Again?'
 
     input = gets.chomp!
     until input.downcase == 'no'
+      puts
       puts 'Ok, here we go again:'
       choice = gets.chomp!
       computer_game(choice)
